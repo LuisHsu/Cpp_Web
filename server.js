e71db5904d9e01ec64fc733c81a1e3e0b4daeddb
LuisHsu <luis8404272@gmail.com>
2014-11-12 03:40:00 +0800

    Parse html to ejs

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 var express=require('express');
+var bodyParser=require('body-parser');
+var fs=require('fs');
+var inspect=require('util').inspect;
 
+/*** Express ***/
 var app=express();
-app.use(express.static(__dirname + '/Pages'));
+
+//Parse json
+app.use(bodyParser.json());
+
+// Set to get ip
+app.enable('trust proxy');
+
+// Set ejs
+app.set('view engine','ejs');
+
+//Serve ejs file
+app.get('/', function(req,res){
+	res.render('pages/index');
+});
+
+app.use(express.static(__dirname+'/resourses'));
+
+// Listen
 app.listen(8080,function(){
-	console.log("Website Started");
+	console.log("Test website Started");
 });
