73c978dc1a681b09379e9fc5448649a5d2e0bb68
LuisHsu <luis8404272@gmail.com>
2014-11-16 01:32:50 +0800

    Basic detail

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var inspect=require('util').inspect;
 //temp variable
 var c=0;
 
-//Database
+// Connect to Database
 var DB=new maria();
 DB.connect({
 		host: "140.116.246.195",
@@ -26,7 +26,7 @@ DB.on('connect',function(){
 		console.log('Database Connect Close');
 	});
 	
-//Category
+// Get Category
 var Category=new Array();
 DB.query('SELECT DISTINCT category FROM Cpp2015.Project_Table;')
 	.on('result',function(res){
@@ -45,7 +45,7 @@ DB.query('SELECT DISTINCT category FROM Cpp2015.Project_Table;')
 /*** Express ***/
 var app=express();
 
-//Parse json
+// Parse json
 app.use(bodyParser.json());
 
 // Set to get ip
@@ -61,6 +61,7 @@ app.get('/', function(req,res){
 
 // Works
 app.get('/works', function(req,res){
+	// Set variable
 	var titles=new Array();
 	var subs=new Array();
 	var auths=new Array();
@@ -73,6 +74,8 @@ app.get('/works', function(req,res){
 	var Class_Download='';
 	var CurWork='';
 	c=0;
+	
+	// Distinguish all and other works
 	if(req.query.work=="all"){
 		CurWork="*";
 		Title_Work="All Works";
@@ -81,15 +84,19 @@ app.get('/works', function(req,res){
 		Title_Work=req.query.work;
 		qstring=qstring+" WHERE category='"+req.query.work+"'";
 	}
+	
+	// Distinguish order option
 	if(req.query.order=="Download"){
-		qstring+=" ORDER BY popularity DESC;";
+		qstring+=" ORDER BY download_times DESC;";
 		Class_Download="sortclicked";
 		Class_Latest="sortbutton";
 	}else{
-		qstring+=" ORDER BY update_time DESC;";
+		qstring+=" ORDER BY upload_time DESC;";
 		Class_Download="sortbutton";
 		Class_Latest="sortclicked";
 	}
+	
+	// Get data and render page
 	DB.query(qstring)
 	.on('result',function(res){
 		res.on('row',function(row){
@@ -97,8 +104,8 @@ app.get('/works', function(req,res){
 			subs[c]=row.subtitle;
 			auths[c]=row.author;
 			descs[c]=row.description;
-			indices[c]=row.index;
-			pics[c]="projects/"+row.index+"/cover";
+			indices[c]=row.project_index;
+			pics[c]="projects/"+row.project_index+"/cover";
 			c+=1;
 		})
 		.on('error',function(err){
@@ -117,6 +124,57 @@ app.get('/works', function(req,res){
 				Class_Download: Class_Download,
 				work:Title_Work,
 				categories:Category
+			});
+	});
+});
+
+// Detail
+app.get('/detail', function(req,res){
+	// Set variable
+	var cover='projects/'+req.query.index+'/cover';
+	var title='';
+	var author='';
+	var subtitle='';
+	var upload_time=0;
+	var star=0;
+	var popularity=0;
+	var download_times=0;
+	var picts=new Array();
+	var description='';
+	
+	// Get data and render page
+	DB.query("SELECT * FROM Cpp2015.Project_Table WHERE project_index="+req.query.index+";")
+	.on('result',function(res){
+		res.on('row',function(row){
+			title=row.title;
+			author=row.author;
+			subtitle=row.subtitle;
+			upload_time=row.upload_time;
+			star=row.star;
+			popularity=row.popularity;
+			download_times=row.download_times;
+			for(var i=0;i<row.pic_count;++i){
+				picts[i]='projects/'+req.query.index+'/snap_'+i;
+			}
+			description=row.description;
+		})
+		.on('error',function(err){
+			console.log(req.ip+': Error- '+inspect(err));
+		})
+	})
+	.on('end',function(){
+		res.render('pages/detail',{
+				title: title,
+				subtitle: subtitle,
+				author: author,
+				description: description,
+				upload_time:upload_time,
+				star:star,
+				popularity:popularity,
+				download_times:download_times,
+				picts:picts,
+				categories:Category,
+				cover:cover
 			});
 	});
 });
